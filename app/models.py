fae58c94e2de9a8c7cbba78454fa334d8dc69d1f
felo <felix.kurgat@student.moringaschool.com>
2022-02-28 16:39:54 +0300

    added class articles

--- app/models.py
+++ app/models.py
@@ -2,7 +2,7 @@ class Sources:
     """
     Sources class to define our sources objects.
     """
-    
+    def __init__(self, id, name, description, category, url, country):
         self.id = id
         self.name = name
         self.description = description
@@ -11,7 +11,7 @@ class Sources:
         self.country = country
 
 
-class Articles:
+
     """
     Articles class to define our articles objects.
     """
