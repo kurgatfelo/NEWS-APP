9bcf3bb52b947820ad960f34a6ad4fd9d8327745
felo <felix.kurgat@student.moringaschool.com>
2022-02-28 16:39:19 +0300

    added init function

--- app/models.py
+++ app/models.py
@@ -2,7 +2,7 @@ class Sources:
     """
     Sources class to define our sources objects.
     """
-    def __init__(self, id, name, description, category, url, country):
+    
         self.id = id
         self.name = name
         self.description = description
